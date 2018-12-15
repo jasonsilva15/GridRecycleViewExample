b884a47292c3732cb6de1d4ac49c27a35c3100b4
jasonsilva15 <Kathe2006>
2018-12-15 12:04:41 -0600

    Commit Tabs

---
+++ app/src/main/java/com/example/service_admin/gridrecycleviewexample/TabsFragmentsAdpter.java
@@ -0,0 +1,22 @@
+package com.example.service_admin.gridrecycleviewexample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsFragmentsAdpter extends FragmentPagerAdapter {
+
+    public TabsFragmentsAdpter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
