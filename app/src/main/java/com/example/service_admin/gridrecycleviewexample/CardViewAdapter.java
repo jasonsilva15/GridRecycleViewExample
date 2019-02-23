6a14ea1bf32edac54b913b36d0a708520b9fae85
jasonsilva15 <Kathe2006>
2019-02-23 09:44:08 -0600

    Commit Tabs

--- app/src/main/java/com/example/service_admin/gridrecycleviewexample/CardViewAdapter.java
+++ app/src/main/java/com/example/service_admin/gridrecycleviewexample/CardViewAdapter.java
@@ -3,7 +3,12 @@ package com.example.service_admin.gridrecycleviewexample;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -23,16 +28,42 @@ public class CardViewAdapter extends  RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View v=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_layout,viewGroup,false);
+        return  new CardHolder(v);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+     Product product = productList.get(i);
+     viewHolder.itemView.setTag(product);
+     CardHolder holder =(CardHolder)viewHolder;
+     holder.textView.setText(product.nombre);
+     holder.imageView.setImageResource(R.mipmap.imag2);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return productList.size();
     }
+
+
+    private class CardHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        ImageView imageView;
+
+        public  CardHolder (View v) {
+            super(v);
+
+            textView = v.findViewById(R.id.cardviewtext);
+            imageView=v.findViewById(R.id.carviewimg);
+        }
+
+    }
+
 }
+
+
+
+
--- app/src/main/java/com/example/service_admin/gridrecycleviewexample/Tab3Fragment.java
+++ app/src/main/java/com/example/service_admin/gridrecycleviewexample/Tab3Fragment.java
@@ -4,16 +4,36 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tab3Fragment  extends Fragment {
+
+    List<Product> productList = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.tab2_fragment,container,false);
+        View view =inflater.inflate(R.layout.tab3_fragment,container,false);
+
+        RecyclerView cardList =view.findViewById(R.id.cardviewlist);
+        cardList.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+        cardList.setLayoutManager(layoutManager);
+
+        productList.add( new Product("Celular" ,"",""));
+        productList.add( new Product("Celular2" ,"",""));
+
+
+        CardViewAdapter adapter = new CardViewAdapter(view.getContext(),productList);
+
+        cardList.setAdapter(adapter);
         return  view ;
     }
 }
